946c1feddd858abe4df10d081fe40ecb05f0a547
Gautam J <gautam.jayapal@gmail.com>
2021-07-28 17:53:01 +0530

    add linear search

---
+++ src/linearSearch.cpp
@@ -0,0 +1,39 @@
+/* CLRS */
+
+/* Time complexity is O(N / 2) for average case, thus O(N) */
+
+/* Time Complexity: O(N)  for both average and worst case*/
+/* Space Complexity: O(1) */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int linearSearch(vector<int> arr, int v) {
+
+    for (int i = 0; i < arr.size(); i++) {
+        if (arr[i] == v)
+            return i;
+    }
+
+    return -1;
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n, v;
+    cin >> n;
+
+    vector<int> arr(n);
+    for (int i = 0; i < arr.size(); i++) {
+        cin >> arr[i];
+    }
+
+    cout << '\n';
+    cin >> v;
+
+    cout << linearSearch(arr, v) << "\n";
+
+    return 0;
+}
