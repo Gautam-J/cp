a344b513072e5bbe52052cbefc88ea39411fbe10
Gautam J <gautam.jayapal@gmail.com>
2021-08-03 09:48:06 +0530

    add max subarray

---
+++ src/maxSubarrayKadane.cpp
@@ -0,0 +1,105 @@
+/* CLRS, LC - Easy */
+/*
+Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+
+A subarray is a contiguous part of an array.
+
+
+
+Example 1:
+
+Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+Output: 6
+Explanation: [4,-1,2,1] has the largest sum = 6.
+Example 2:
+
+Input: nums = [1]
+Output: 1
+Example 3:
+
+Input: nums = [5,4,-1,7,8]
+Output: 23
+
+
+Constraints:
+
+1 <= nums.length <= 3 * 104
+-105 <= nums[i] <= 105
+*/
+
+/* The same problem can be done in (N lg N) also. */
+
+/* Time Complexity: O(N) */
+/* Space Complexity: O(1) */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> findMaxSubarray(vector<int>& arr) {
+    vector<int> result;
+    int bestLeft, bestRight, temp;
+
+    int maxSum = INT_MIN;
+    int left = 0;
+    int sum = 0;
+
+    for (int i = 0; i < arr.size(); i++) {
+       temp = sum + arr[i];
+
+       // sum = max(temp, arr[i])
+       if (temp > arr[i]) {
+           sum = temp;
+       } else {  // after adding A[j], if the sum value decreases, then we start a new subarray
+           sum = arr[i];
+           left = i;
+       }
+
+       // maxSum = max(maxSum, sum)
+       if (maxSum < sum) {
+           maxSum = sum;
+           bestLeft = left;
+           bestRight = i;
+       }
+    }
+
+    result.push_back(bestLeft);
+    result.push_back(bestRight);
+    result.push_back(maxSum);
+
+    return result;
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+
+    // get user input
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    // calculate maximum subarray
+    vector<int> result = findMaxSubarray(arr);
+
+    int leftIndex = result[0];
+    int rightIndex = result[1];
+    int sumOfMaxSubarray = result[2];
+
+    auto start = arr.begin() + leftIndex;
+    auto end = arr.begin() + leftIndex + rightIndex + 1;
+
+    vector<int> maxSubarray(rightIndex - leftIndex + 1);
+    copy(start, end, maxSubarray.begin());
+
+    // print the maxSubarray and its sum
+    cout << "Maximum Subarray: ";
+    for (int i = 0; i < maxSubarray.size(); i++)
+        cout << maxSubarray[i] << ' ';
+
+    cout << '\n' << "Sum: " << sumOfMaxSubarray << '\n';
+
+    return 0;
+}
--- src/twoSum.cpp
+++ src/twoSum.cpp
@@ -1,6 +1,37 @@
-/* CLRS */
+/* CLRS, LC - Easy */
+/*
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 
-/* Determine if there exists two elements in S such that they add upto x */
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
+
+
+Constraints:
+
+2 <= nums.length <= 104
+-109 <= nums[i] <= 109
+-109 <= target <= 109
+Only one valid answer exists.
+*/
+
+/* Using hashmaps, we can solve it in O(N) time. */
 
 /* Time Complexity: O(N lg N) */
 /* Space Complexity: O(N) */
@@ -8,7 +39,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool twoSum(vector<int>& arr, int x) {
+vector<int> twoSum(vector<int>& arr, int x) {
+    vector<int> result;
+
     // sort the given array O(n lg n)
     sort(arr.begin(), arr.end());
 
@@ -16,8 +49,12 @@ bool twoSum(vector<int>& arr, int x) {
     int low = 0;
     int high = arr.size() - 1;
     while (low < high) {
-        if (arr[low] + arr[high] == x)
-            return true;
+
+        if (arr[low] + arr[high] == x) {
+            result.push_back(low);
+            result.push_back(high);
+            return result;
+        }
 
         if (arr[low] + arr[high] > x)
             high--;
@@ -25,7 +62,8 @@ bool twoSum(vector<int>& arr, int x) {
             low++;
     }
 
-    return false;
+    // not necessary
+    return result;
 }
 
 int main() {
@@ -42,7 +80,14 @@ int main() {
 
     cin >> x;
 
-    cout << twoSum(arr, x) << "\n";
+    // get indices
+    vector<int> result = twoSum(arr, x);
+
+    // output
+    for (auto x : result) {
+        cout << x << ' ';
+    }
+    cout << '\n';
 
     return 0;
 }
