776115726c9a64ffa12a2fe3e11c9774c2a7e888
Gautam J <gautam.jayapal@gmail.com>
2021-08-14 22:19:03 +0530

    update template

--- src/template.cpp
+++ src/template.cpp
@@ -8,6 +8,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#define FOR(i, n) for (int i = 0; i < n; i++)
+#define all(a) a.begin(), a.end()
+
+typedef long long ll;
+typedef vector<int> vi;
+
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
