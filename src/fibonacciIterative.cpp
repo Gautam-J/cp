a04d476f7926675746ebefb5fb94923665a88d54
Gautam J <gautam.jayapal@gmail.com>
2021-08-18 18:29:17 +0530

    update fibonacci LC

--- src/fibonacciIterative.cpp
+++ src/fibonacciIterative.cpp
@@ -1,8 +1,37 @@
-/* Practice */
+/* LC - Easy */
 /*
-Given an integer n, print the nth fibinacci number.
-0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, ...
+The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, such that each number is the sum of the two preceding ones, starting from 0 and 1. That is,
+
+F(0) = 0, F(1) = 1
+F(n) = F(n - 1) + F(n - 2), for n > 1.
+Given n, calculate F(n).
+
+
+
+Example 1:
+
+Input: n = 2
+Output: 1
+Explanation: F(2) = F(1) + F(0) = 1 + 0 = 1.
+Example 2:
+
+Input: n = 3
+Output: 2
+Explanation: F(3) = F(2) + F(1) = 1 + 1 = 2.
+Example 3:
+
+Input: n = 4
+Output: 3
+Explanation: F(4) = F(3) + F(2) = 2 + 1 = 3.
+
+
+Constraints:
+
+0 <= n <= 30
 */
+
+/* The series is as follows: */
+/* 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, ... */
 
 /* Time Complexity: O(n) */
 /* Space Complexity: O(1) */
@@ -16,17 +45,15 @@ using namespace std;
 typedef long long ll;
 typedef vector<int> vi;
 
-ll getFib(ll n) {
+int fib(int n) {
 
-    if (n == 1)
-        return 0;
-    if (n == 2)
-        return 1;
+    if (n < 2)
+        return n;
 
-    ll a = 0;
-    ll b = 1;
-    ll c = 0;
-    for (int i = 3; i <= n; i++) {
+    int a = 0;
+    int b = 1;
+    int c;
+    for (int i = 2; i <= n; i++) {
         c = a + b;
         a = b;
         b = c;
@@ -39,9 +66,9 @@ int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
 
-    ll n;
+    int n;
     cin >> n;
-    cout << getFib(n) << '\n';
+    cout << fib(n) << '\n';
 
     return 0;
 }
