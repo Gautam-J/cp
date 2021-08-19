e4aed272624c959fb3ae01959fcac973024d6cc6
Gautam J <gautam.jayapal@gmail.com>
2021-08-19 09:30:05 +0530

    update template

--- src/template.cpp
+++ src/template.cpp
@@ -10,15 +10,18 @@ using namespace std;
 
 #define FOR(i, n) for (int i = 0; i < n; i++)
 #define all(a) a.begin(), a.end()
+#define trav(a, x) for (auto& a : x)
 
 typedef long long ll;
 typedef vector<int> vi;
+
+const char nl = '\n';
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
 
-    cout << "Hello world!" << "\n";
+    cout << "Hello world!" << nl;
 
     return 0;
 }
