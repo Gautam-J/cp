0fada97c5c26cce9b659e7d66833b01aea759d84
Gautam J <gautam.jayapal@gmail.com>
2021-08-21 15:28:31 +0530

    add sample 800 prob

---
+++ src/iWannaBeTheGuy.cpp
@@ -0,0 +1,88 @@
+/* CF - 800 */
+/*
+A. I Wanna Be the Guy
+time limit per test1 second
+memory limit per test256 megabytes
+inputstandard input
+outputstandard output
+There is a game called "I Wanna Be the Guy", consisting of n levels. Little X and his friend Little Y are addicted to the game. Each of them wants to pass the whole game.
+
+Little X can pass only p levels of the game. And Little Y can pass only q levels of the game. You are given the indices of levels Little X can pass and the indices of levels Little Y can pass. Will Little X and Little Y pass the whole game, if they cooperate each other?
+
+Input
+The first line contains a single integer n (1 ≤  n ≤ 100).
+
+The next line contains an integer p (0 ≤ p ≤ n) at first, then follows p distinct integers a1, a2, ..., ap (1 ≤ ai ≤ n). These integers denote the indices of levels Little X can pass. The next line contains the levels Little Y can pass in the same format. It's assumed that levels are numbered from 1 to n.
+
+Output
+If they can pass all the levels, print "I become the guy.". If it's impossible, print "Oh, my keyboard!" (without the quotes).
+
+Examples
+inputCopy
+4
+3 1 2 3
+2 2 4
+outputCopy
+I become the guy.
+inputCopy
+4
+3 1 2 3
+2 2 3
+outputCopy
+Oh, my keyboard!
+Note
+In the first sample, Little X can pass levels [1 2 3], and Little Y can pass level [2 4], so they can pass all the levels both.
+
+In the second sample, no one can pass level 4.
+*/
+
+/* Time Complexity: O() */
+/* Space Complexity: O() */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FOR(i, n) for (int i = 0; i < n; i++)
+#define all(a) a.begin(), a.end()
+#define trav(i, a) for (auto& i : a)
+
+typedef long long ll;
+typedef vector<int> vi;
+
+mt19937 mt_rand(chrono::high_resolution_clock::now().time_since_epoch().count());
+const char nl = '\n';
+
+bool canPass(int n, vi& x, vi& y) {
+    unordered_set<int> seen;
+
+    trav(i, x)
+        seen.insert(i);
+    trav(i, y)
+        seen.insert(i);
+
+    return n == (int)seen.size();
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n, p;
+    cin >> n;
+    cin >> p;
+    vi x(p);
+    trav(i, x)
+        cin >> i;
+    cin >> p;
+    vi y(p);
+    trav(i, y)
+        cin >> i;
+
+    cout << (canPass(n, x, y) ? "I become the guy." : "Oh, my keyboard!") << nl;
+
+#ifdef _GLIBCXX_DEBUG
+    cerr << endl << "finished in " << clock() * 1.0 / CLOCKS_PER_SEC << " sec" << endl;
+#endif
+
+    return 0;
+}
