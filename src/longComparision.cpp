7a59dde487985d9570c7b2b90e7e7ec3e94574ca
Gautam J <gautam.jayapal@gmail.com>
2021-12-11 10:39:36 +0530

    add probs

---
+++ src/longComparision.cpp
@@ -0,0 +1,112 @@
+
+/*
+A. Long Comparison
+time limit per test2 seconds
+memory limit per test256 megabytes
+inputstandard input
+outputstandard output
+Monocarp wrote down two numbers on a whiteboard. Both numbers follow a specific format: a positive integer x with p zeros appended to its end.
+
+Now Monocarp asks you to compare these two numbers. Can you help him?
+
+Input
+The first line contains a single integer t (1≤t≤104) — the number of testcases.
+
+The first line of each testcase contains two integers x1 and p1 (1≤x1≤106;0≤p1≤106) — the description of the first number.
+
+The second line of each testcase contains two integers x2 and p2 (1≤x2≤106;0≤p2≤106) — the description of the second number.
+
+Output
+For each testcase print the result of the comparison of the given two numbers. If the first number is smaller than the second one, print '<'. If the first number is greater than the second one, print '>'. If they are equal, print '='.
+
+Example
+inputCopy
+5
+2 1
+19 0
+10 2
+100 1
+1999 0
+2 3
+1 0
+1 0
+99 0
+1 2
+outputCopy
+>
+=
+<
+=
+<
+Note
+The comparisons in the example are: 20>19, 1000=1000, 1999<2000, 1=1, 99<100.
+*/
+
+/* Time Complexity: O(1) */
+/* Space Complexity: O(1) */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FOR(i, n) for (int i = 0; i < n; i++)
+#define all(a) a.begin(), a.end()
+#define sz(x) (int)(x).size()
+#define trav(i, a) for (auto& i : a)
+
+#ifdef _GLIBCXX_DEBUG
+#define debug(x) cerr << "\e[91m"<<"[DEBUG] "<<__func__<<":"<<__LINE__<<" [" << #x << "] = [" << x << "]" << "\e[39m" << '\n';
+#else
+#define debug(x)
+#endif
+
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef vector<int> vi;
+typedef vector<pi> vpi;
+
+mt19937 mt_rand(chrono::high_resolution_clock::now().time_since_epoch().count());
+const char nl = '\n';
+const double PI = acos(-1);
+
+void solve(ll x1, ll p1, ll x2, ll p2) {
+    // remove the common powers
+    ll minimum = min(p1, p2);
+    p1 -= minimum;
+    p2 -= minimum;
+
+    if (p1 >= 7)
+        cout << ">" << nl;
+    else if (p2 >= 7)
+        cout << "<" << nl;
+    else {
+        x1 *= pow(10, p1);
+        x2 *= pow(10, p2);
+
+        if (x1 > x2)
+            cout << ">" << nl;
+        else if (x1 < x2)
+            cout << "<" << nl;
+        else
+            cout << "=" << nl;
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t;
+    cin >> t;
+
+    ll x1, p1, x2, p2;
+    while (t--) {
+        cin >> x1 >> p1 >> x2 >> p2;
+        solve(x1, p1, x2, p2);
+    }
+
+#ifdef _GLIBCXX_DEBUG
+    cerr << endl << "finished in " << clock() * 1.0 / CLOCKS_PER_SEC << " sec" << endl;
+#endif
+
+    return 0;
+}
---
+++ src/twoSum2.cpp
@@ -0,0 +1,107 @@
+/* LC - Easy */
+/*
+Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, find two numbers such that they add up to a specific target number. Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 <= numbers.length.
+
+Return the indices of the two numbers, index1 and index2, added by one as an integer array [index1, index2] of length 2.
+
+The tests are generated such that there is exactly one solution. You may not use the same element twice.
+
+
+
+Example 1:
+
+Input: numbers = [2,7,11,15], target = 9
+Output: [1,2]
+Explanation: The sum of 2 and 7 is 9. Therefore, index1 = 1, index2 = 2. We return [1, 2].
+Example 2:
+
+Input: numbers = [2,3,4], target = 6
+Output: [1,3]
+Explanation: The sum of 2 and 4 is 6. Therefore index1 = 1, index2 = 3. We return [1, 3].
+Example 3:
+
+Input: numbers = [-1,0], target = -1
+Output: [1,2]
+Explanation: The sum of -1 and 0 is -1. Therefore index1 = 1, index2 = 2. We return [1, 2].
+
+
+Constraints:
+
+2 <= numbers.length <= 3 * 104
+-1000 <= numbers[i] <= 1000
+numbers is sorted in non-decreasing order.
+-1000 <= target <= 1000
+The tests are generated such that there is exactly one solution.
+*/
+
+/* Time Complexity: O(log n) */
+/* Space Complexity: O(1) */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FOR(i, n) for (int i = 0; i < n; i++)
+#define all(a) a.begin(), a.end()
+#define sz(x) (int)(x).size()
+#define trav(i, a) for (auto& i : a)
+
+#ifdef _GLIBCXX_DEBUG
+#define debug(x) cerr << "\e[91m"<<"[DEBUG] "<<__func__<<":"<<__LINE__<<" [" << #x << "] = [" << x << "]" << "\e[39m" << '\n';
+#else
+#define debug(x)
+#endif
+
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef vector<int> vi;
+typedef vector<pi> vpi;
+
+mt19937 mt_rand(chrono::high_resolution_clock::now().time_since_epoch().count());
+const char nl = '\n';
+const double PI = acos(-1);
+
+vector<int> twoSum(vector<int>& numbers, int target) {
+    int low = 0, high = numbers.size() - 1, currSum;
+    vector<int> res;
+
+    while (low <= high) {
+        currSum = numbers[low] + numbers[high];
+
+        if (currSum == target) {
+            res.push_back(low + 1);
+            res.push_back(high + 1);
+            return res;
+        }
+
+        if (currSum < target)
+            low++;
+        else if (currSum > target)
+            high--;
+    }
+
+    return res;
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n, target;
+    cin >> n;
+
+    vi a(n);
+    trav(i, a)
+        cin >> i;
+
+    cin >> target;
+    vi res = twoSum(a, target);
+    trav(j, res)
+        cout << j << " ";
+    cout << nl;
+
+#ifdef _GLIBCXX_DEBUG
+    cerr << endl << "finished in " << clock() * 1.0 / CLOCKS_PER_SEC << " sec" << endl;
+#endif
+
+    return 0;
+}
