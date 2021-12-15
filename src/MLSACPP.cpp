aa1339a51b13edc64a457181e5394aadbcfeb907
Gautam J <gautam.jayapal@gmail.com>
2021-12-15 22:44:28 +0530

    add MLSA probs

---
+++ src/MLSACPP.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    try {
+       throw 10;
+    } catch (...) {
+        cout << "default exceptionn";
+    } catch (int param) {
+        cout << "int exceptionn";
+    }
+
+    return 0;
+}
---
+++ src/MLSASets.cpp
@@ -0,0 +1,59 @@
+
+/*
+
+*/
+
+/* Time Complexity: O() */
+/* Space Complexity: O() */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FOR(i, n) for (int i = 0; i < n; i++)
+#define all(a) a.begin(), a.end()
+#define sz(x) (int)(x).size()
+#define trav(i, a) for (auto& i : a)
+
+#ifdef _GLIBCXX_DEBUG
+#define debug(x) cerr << "\e[91m"<<"[DEBUG] "<<__func__<<":"<<__LINE__<<" [" << #x << "] = [" << x << "]" << "\e[39m" << '\n';
+#else
+#define debug(x)
+#endif
+
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef vector<int> vi;
+typedef vector<pi> vpi;
+
+mt19937 mt_rand(chrono::high_resolution_clock::now().time_since_epoch().count());
+const char nl = '\n';
+const double PI = acos(-1);
+
+int solve(string& s) {
+    unordered_set<char> cnt;
+
+    trav(i, s)
+        cnt.insert(i);
+
+    return sz(cnt);
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t, n;
+    string s;
+    cin >> t;
+
+    while (t--) {
+        cin >> n >> s;
+        cout << solve(s) << nl;
+    }
+
+#ifdef _GLIBCXX_DEBUG
+    cerr << endl << "finished in " << clock() * 1.0 / CLOCKS_PER_SEC << " sec" << endl;
+#endif
+
+    return 0;
+}
