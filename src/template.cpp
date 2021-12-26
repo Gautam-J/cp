5027766560d0939c9441833a79c987d974eee086
Gautam J <gautam.jayapal@gmail.com>
2021-12-26 15:43:28 +0530

    update template

--- src/template.cpp
+++ src/template.cpp
@@ -9,29 +9,49 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#pragma GCC optimize("Ofast,unroll-loops")
+#pragma GCC target("avx,avx2,fma")
+
+void dbg_out() { cerr << endl; }
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+
+template<typename T> T gcd(T a, T b) { return ( b ? __gcd(a, b) : a); }
+template<typename T> T lcm(T a, T b) { return (a * (b / gcd(a, b))); }
+
 #define FOR(i, n) for (int i = 0; i < n; i++)
 #define all(a) a.begin(), a.end()
 #define sz(x) (int)(x).size()
 #define trav(i, a) for (auto& i : a)
+#define pb push_back
+#define SHUF(v) shuffle(all(v), mt_rand)
+#define umap unordered_map
+#define uset unordered_set
+#define imax INT_MAX
+#define imin INT_MIN
 
 #ifdef _GLIBCXX_DEBUG
-#define debug(x) cerr << "\e[91m"<<"[DEBUG] "<<__func__<<":"<<__LINE__<<" [" << #x << "] = [" << x << "]" << "\e[39m" << '\n';
+#define debug(...) cerr << "[DEBUG]: [" << #__VA_ARGS__ << "]:", dbg_out(__VA_ARGS__)
 #else
-#define debug(x)
+#define debug(...)
 #endif
 
 typedef long long ll;
 typedef pair<int, int> pi;
 typedef vector<int> vi;
+typedef vector<vi> vvi;
 typedef vector<pi> vpi;
 
+// mt19937_64 for 64 bit random numbers
 mt19937 mt_rand(chrono::high_resolution_clock::now().time_since_epoch().count());
+
 const char nl = '\n';
 const double PI = acos(-1);
 
 int main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
 
     cout << "Hello world!" << nl;
 
