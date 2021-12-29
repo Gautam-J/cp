64fdb9542d3e4df1ae59641f73e8eebb675e8587
Gautam J <gautam.jayapal@gmail.com>
2021-12-29 13:20:25 +0530

    add MLSA prob

---
+++ src/MLSABinarySearch2.cpp
@@ -0,0 +1,93 @@
+
+/*
+
+*/
+
+/* Time Complexity: O() */
+/* Space Complexity: O() */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#pragma GCC optimize("Ofast,unroll-loops")
+#pragma GCC target("avx,avx2,fma")
+
+void dbg_out() { cerr << endl; }
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+
+template<typename T> T gcd(T a, T b) { return ( b ? __gcd(a, b) : a); }
+template<typename T> T lcm(T a, T b) { return (a * (b / gcd(a, b))); }
+
+#define FOR(i, n) for (int i = 0; i < n; i++)
+#define all(a) a.begin(), a.end()
+#define sz(x) (int)(x).size()
+#define trav(i, a) for (auto& i : a)
+#define pb push_back
+#define SHUF(v) shuffle(all(v), mt_rand)
+#define umap unordered_map
+#define uset unordered_set
+#define imax INT_MAX
+#define imin INT_MIN
+
+#ifdef _GLIBCXX_DEBUG
+#define debug(...) cerr << "[DEBUG]: [" << #__VA_ARGS__ << "]:", dbg_out(__VA_ARGS__)
+#else
+#define debug(...)
+#endif
+
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef vector<int> vi;
+typedef vector< vi > vvi;
+typedef vector< pi > vpi;
+
+// mt19937_64 for 64 bit random numbers
+mt19937 mt_rand(chrono::high_resolution_clock::now().time_since_epoch().count());
+
+const char nl = '\n';
+const double PI = acos(-1);
+
+int solve(vi& a) {
+    int low = 0, high = sz(a) - 1, mid;
+
+    int res = sz(a);
+    while (low <= high) {
+        mid = low + (high - low) / 2;
+
+        if (a[mid] == 1) {
+            res = mid;
+            high = mid - 1;
+        } else if (a[mid] < 1) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+
+    return sz(a) - res;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t, n;
+    cin >> t;
+
+    while (t--) {
+        cin >> n;
+        vi a(n);
+        trav(i, a)
+            cin >> i;
+
+        cout << solve(a) << nl;
+    }
+
+#ifdef _GLIBCXX_DEBUG
+    cerr << endl << "finished in " << clock() * 1.0 / CLOCKS_PER_SEC << " sec" << endl;
+#endif
+
+    return 0;
+}
