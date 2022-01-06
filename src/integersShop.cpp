b349e7263e6a3f1681d0fd932903eced40e1a2e8
Gautam J <gautam.jayapal@gmail.com>
2022-01-06 10:05:06 +0530

    add contest probs

---
+++ src/integersShop.cpp
@@ -0,0 +1,168 @@
+
+/*
+B. Integers Shop
+time limit per test2 seconds
+memory limit per test256 megabytes
+inputstandard input
+outputstandard output
+The integers shop sells n segments. The i-th of them contains all integers from li to ri and costs ci coins.
+
+Tomorrow Vasya will go to this shop and will buy some segments there. He will get all integers that appear in at least one of bought segments. The total cost of the purchase is the sum of costs of all segments in it.
+
+After shopping, Vasya will get some more integers as a gift. He will get integer x as a gift if and only if all of the following conditions are satisfied:
+
+Vasya hasn't bought x.
+Vasya has bought integer l that is less than x.
+Vasya has bought integer r that is greater than x.
+Vasya can get integer x as a gift only once so he won't have the same integers after receiving a gift.
+
+For example, if Vasya buys segment [2,4] for 20 coins and segment [7,8] for 22 coins, he spends 42 coins and receives integers 2,3,4,7,8 from these segments. He also gets integers 5 and 6 as a gift.
+
+Due to the technical issues only the first s segments (that is, segments [l1,r1],[l2,r2],…,[ls,rs]) will be available tomorrow in the shop.
+
+Vasya wants to get (to buy or to get as a gift) as many integers as possible. If he can do this in differents ways, he selects the cheapest of them.
+
+For each s from 1 to n, find how many coins will Vasya spend if only the first s segments will be available.
+
+Input
+The first line contains a single integer t (1≤t≤1000) — the number of test cases.
+
+The first line of each test case contains the single integer n (1≤n≤105) — the number of segments in the shop.
+
+Each of next n lines contains three integers li, ri, ci (1≤li≤ri≤109,1≤ci≤109) — the ends of the i-th segments and its cost.
+
+It is guaranteed that the total sum of n over all test cases doesn't exceed 2⋅105.
+
+Output
+For each test case output n integers: the s-th (1≤s≤n) of them should be the number of coins Vasia will spend in the shop if only the first s segments will be available.
+
+Example
+inputCopy
+3
+2
+2 4 20
+7 8 22
+2
+5 11 42
+5 11 42
+6
+1 4 4
+5 8 9
+7 8 7
+2 10 252
+1 11 271
+1 10 1
+outputCopy
+20
+42
+42
+42
+4
+13
+11
+256
+271
+271
+Note
+In the first test case if s=1 then Vasya can buy only the segment [2,4] for 20 coins and get 3 integers.
+
+The way to get 7 integers for 42 coins in case s=2 is described in the statement.
+
+In the second test case note, that there can be the same segments in the shop.
+*/
+
+/* Time Complexity: O(n) */
+/* Space Complexity: O(1) */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#pragma GCC optimize("Ofast,unroll-loops")
+#pragma GCC target("avx,avx2,fma")
+
+void dbg_out() { cerr << endl; }
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+
+template<typename T> T gcd(T a, T b) { return ( b ? __gcd(a, b) : a); }
+template<typename T> T lcm(T a, T b) { return (a * (b / gcd(a, b))); }
+
+#define FOR(i, n) for (int i = 0; i < n; i++)
+#define all(a) a.begin(), a.end()
+#define sz(x) (int)(x).size()
+#define trav(i, a) for (auto& i : a)
+#define pb push_back
+#define SHUF(v) shuffle(all(v), mt_rand)
+#define umap unordered_map
+#define uset unordered_set
+#define imax INT_MAX
+#define imin INT_MIN
+
+#ifdef _GLIBCXX_DEBUG
+#define debug(...) cerr << "[DEBUG]: [" << #__VA_ARGS__ << "]:", dbg_out(__VA_ARGS__)
+#else
+#define debug(...)
+#endif
+
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef vector<int> vi;
+typedef vector< vi > vvi;
+typedef vector< pi > vpi;
+
+// mt19937_64 for 64 bit random numbers
+mt19937 mt_rand(chrono::high_resolution_clock::now().time_since_epoch().count());
+
+const char nl = '\n';
+const double PI = acos(-1);
+
+void solve(vvi& a) {
+    int l, r, c, ans;
+
+    pi low = { imax, imax };
+    pi high = { imin, imin };
+    pi single = { imin, imin };
+
+    trav(i, a) {
+        l = i[0];
+        r = i[1];
+        c = i[2];
+
+        low = min(low, { l, c });
+        high = max(high, { r, -c });
+        single = max(single, { r - l, -c });
+
+        ans = low.second - high.second;
+        if (single.first == high.first - low.first)
+            ans = min(ans, -single.second);
+
+        cout << ans << nl;
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t, n;
+    cin >> t;
+
+    while (t--) {
+        cin >> n;
+        vvi a(n, vi(3, 0));
+
+        trav(i, a) {
+            trav(j, i) {
+                cin >> j;
+            }
+        }
+        solve(a);
+    }
+
+#ifdef _GLIBCXX_DEBUG
+    cerr << endl << "finished in " << clock() * 1.0 / CLOCKS_PER_SEC << " sec" << endl;
+#endif
+
+    return 0;
+}
---
+++ src/stableArrangementOfRooks.cpp
@@ -0,0 +1,140 @@
+
+/*
+A. Stable Arrangement of Rooks
+time limit per test1 second
+memory limit per test256 megabytes
+inputstandard input
+outputstandard output
+You have an n×n chessboard and k rooks. Rows of this chessboard are numbered by integers from 1 to n from top to bottom and columns of this chessboard are numbered by integers from 1 to n from left to right. The cell (x,y) is the cell on the intersection of row x and collumn y for 1≤x≤n and 1≤y≤n.
+
+The arrangement of rooks on this board is called good, if no rook is beaten by another rook.
+
+A rook beats all the rooks that shares the same row or collumn with it.
+
+The good arrangement of rooks on this board is called not stable, if it is possible to move one rook to the adjacent cell so arrangement becomes not good. Otherwise, the good arrangement is stable. Here, adjacent cells are the cells that share a side.
+
+Such arrangement of 3 rooks on the 4×4 chessboard is good, but it is not stable: the rook from (1,1) can be moved to the adjacent cell (2,1) and rooks on cells (2,1) and (2,4) will beat each other.
+Please, find any stable arrangement of k rooks on the n×n chessboard or report that there is no such arrangement.
+
+Input
+The first line contains a single integer t (1≤t≤100) — the number of test cases.
+
+The first line of each test case contains two integers n, k (1≤k≤n≤40) — the size of the chessboard and the number of rooks.
+
+Output
+If there is a stable arrangement of k rooks on the n×n chessboard, output n lines of symbols . and R. The j-th symbol of the i-th line should be equals R if and only if there is a rook on the cell (i,j) in your arrangement.
+
+If there are multiple solutions, you may output any of them.
+
+If there is no stable arrangement, output −1.
+
+Example
+inputCopy
+5
+3 2
+3 3
+1 1
+5 2
+40 33
+outputCopy
+..R
+...
+R..
+-1
+R
+.....
+R....
+.....
+....R
+.....
+-1
+Note
+In the first test case, you should find stable arrangement of 2 rooks on the 3×3 chessboard. Placing them in cells (3,1) and (1,3) gives stable arrangement.
+
+In the second test case it can be shown that it is impossbile to place 3 rooks on the 3×3 chessboard to get stable arrangement.
+
+*/
+
+/* Time Complexity: O(k) */
+/* Space Complexity: O(1) */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#pragma GCC optimize("Ofast,unroll-loops")
+#pragma GCC target("avx,avx2,fma")
+
+void dbg_out() { cerr << endl; }
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+
+template<typename T> T gcd(T a, T b) { return ( b ? __gcd(a, b) : a); }
+template<typename T> T lcm(T a, T b) { return (a * (b / gcd(a, b))); }
+
+#define FOR(i, n) for (int i = 0; i < n; i++)
+#define all(a) a.begin(), a.end()
+#define sz(x) (int)(x).size()
+#define trav(i, a) for (auto& i : a)
+#define pb push_back
+#define SHUF(v) shuffle(all(v), mt_rand)
+#define umap unordered_map
+#define uset unordered_set
+#define imax INT_MAX
+#define imin INT_MIN
+
+#ifdef _GLIBCXX_DEBUG
+#define debug(...) cerr << "[DEBUG]: [" << #__VA_ARGS__ << "]:", dbg_out(__VA_ARGS__)
+#else
+#define debug(...)
+#endif
+
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef vector<int> vi;
+typedef vector< vi > vvi;
+typedef vector< pi > vpi;
+
+// mt19937_64 for 64 bit random numbers
+mt19937 mt_rand(chrono::high_resolution_clock::now().time_since_epoch().count());
+
+const char nl = '\n';
+const double PI = acos(-1);
+
+void solve(int n, int k) {
+    if ((ceil((float)n / 2.0)) < k) {  //  ceil(n / 2) OR floor((n + 1) / 2)
+        cout << -1 << nl;
+        return;
+    }
+
+    vector<vector<char> > a(n, vector<char>(n, '.'));
+
+    for (int i = 0; i < k; i++)
+        a[2 * i][2 * i] = 'R';
+
+    trav(i, a) {
+        trav(j, i)
+            cout << j;
+        cout << nl;
+    }
+
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t, n, k;
+    cin >> t;
+
+    while (t--) {
+        cin >> n >> k;
+        solve(n, k);
+    }
+
+#ifdef _GLIBCXX_DEBUG
+    cerr << endl << "finished in " << clock() * 1.0 / CLOCKS_PER_SEC << " sec" << endl;
+#endif
+
+    return 0;
+}
