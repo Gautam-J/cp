d190e6ab42d4a5ca281a7ba9e9db9eaf69852b05
Gautam J <gautam.jayapal@gmail.com>
2022-03-21 19:32:14 +0530

    update bubbleSort best case

--- src/bubbleSort.cpp
+++ src/bubbleSort.cpp
@@ -1,10 +1,28 @@
 /* CLRS */
 
-/* Time Complexity: O(N^2) worst and best case */
+/* Time Complexity: O(N^2) worst and best case can be O(n) */
 /* Space Complexity: O(1) since we are doing it inplace */
 
 #include <bits/stdc++.h>
+#include <set>
 using namespace std;
+
+void bubbleSortImproved(vector<int>& arr) {
+    // stops after one full pass of the array if already sorted.
+    // that is, best case time Complexity becomes O(n), instead of O(n^2)
+
+    for (int i = 0; i < arr.size() - 1; i++) {
+        bool swapped = false;
+        for (int j = 0; j < arr.size() - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swapped = true;
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+        if (!swapped)
+            return;
+    }
+}
 
 void bubbleSort(vector<int>& arr) {
     for (int i = 0; i < arr.size() - 1; i++) {
@@ -30,7 +48,8 @@ int main() {
     }
 
     // sort array
-    bubbleSort(arr);
+    /* bubbleSort(arr); */
+    bubbleSortImproved(arr);
 
     // print arr
     for (int i = 0; i < n; i++) {
