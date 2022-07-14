f80097b177bbcf807b2776eca62a2d7338120d06
Gautam J <gautam.jayapal@gmail.com>
2022-07-14 23:13:39 +0530

    update template

--- src/template.cpp
+++ src/template.cpp
@@ -19,7 +19,7 @@ template<typename T> T lcm(T a, T b) { return (a * (b / gcd(a, b))); }
 #define allit(a) a.begin(), a.end()
 #define sz(x) (int)(x).size()
 #define pb push_back
-#define SHUF(v) shuffle(all(v), mt_rand)
+#define shuf(v) shuffle(all(v), rng)
 #define umap unordered_map
 #define uset unordered_set
 #define imax INT_MAX
@@ -38,27 +38,26 @@ typedef vector<int> vi;
 typedef vector< vi > vvi;
 typedef vector< pi > vpi;
 
-// mt19937_64 for 64 bit random numbers
-mt19937 mt_rand(chrono::high_resolution_clock::now().time_since_epoch().count());
+mt19937 rng(chrono::high_resolution_clock::now().time_since_epoch().count());
 
 const char nl = '\n';
 /* const ld PI = acos(-1.0); */
 
-int solve() {
+void solve() {
     // Algorithm:
     //
     //
     // Time: O()
     // Space: O()
 
-    return -1;
+    cout << "Hello, world!" << nl;
 }
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    cout << "Hello, world!" << nl;
+    solve();
 
 #ifdef _GLIBCXX_DEBUG
     cerr << endl << "finished in " << clock() * 1.0 / CLOCKS_PER_SEC << " sec" << endl;
