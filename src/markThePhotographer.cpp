bc96032a8b9d8d072e6731c7c97ee4ff81fe78d8
Gautam J <gautam.jayapal@gmail.com>
2022-07-15 22:37:35 +0530

    add contest problems

---
+++ src/markThePhotographer.cpp
@@ -0,0 +1,96 @@
+/* CF */
+/* Time Complexity: O(nlogn) */
+/* Space Complexity: O(1) */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void dbg_out() { cerr << endl; }
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+
+template<typename T> T gcd(T a, T b) { return ( b ? __gcd(a, b) : a); }
+template<typename T> T lcm(T a, T b) { return (a * (b / gcd(a, b))); }
+
+#define forn(i, l, r) for (int i = (int)l; i < (int)r; ++i)
+#define fore(i, l, r) for (int i = (int)l; i <= (int)r; ++i)
+#define trav(i, a) for (auto& i : a)
+#define allit(a) a.begin(), a.end()
+#define sz(x) (int)(x).size()
+#define pb push_back
+#define shuf(v) shuffle(all(v), rng)
+#define umap unordered_map
+#define uset unordered_set
+#define imax INT_MAX
+#define imin INT_MIN
+
+#ifdef _GLIBCXX_DEBUG
+#define debug(...) cerr << "[DEBUG]: [" << #__VA_ARGS__ << "]:", dbg_out(__VA_ARGS__)
+#else
+#define debug(...)
+#endif
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pi;
+typedef vector<int> vi;
+typedef vector< vi > vvi;
+typedef vector< pi > vpi;
+
+mt19937 rng(chrono::high_resolution_clock::now().time_since_epoch().count());
+
+const char nl = '\n';
+/* const ld PI = acos(-1.0); */
+
+void solve() {
+    // Algorithm:
+    // Sort the array
+    // First half will be the front row
+    // Second half will be the back row
+    // Thus, compare each person in same position in front row
+    // and back row, and check for constraint.
+    //
+    // Time: O(nlogn)
+    // Space: O(1)
+
+    int n, x;
+    cin >> n >> x;
+    vi a(2*n);
+    trav(i, a)
+        cin >> i;
+
+    sort(allit(a));
+    int i = 0, j = n - 1, res = 0;
+    while (i < n && j < 2 * n) {
+        if (a[j] - a[i] >= x) {
+            res++;
+            i++;
+            j++;
+        } else {
+            j++;
+        }
+    }
+
+    if (res == n)
+        cout << "YES";
+    else
+        cout << "NO";
+    cout << nl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
+
+#ifdef _GLIBCXX_DEBUG
+    cerr << endl << "finished in " << clock() * 1.0 / CLOCKS_PER_SEC << " sec" << endl;
+#endif
+
+    return 0;
+}
