b95c92f5b222ee0cf40f3833c00183b17f2f4804
Gautam J <gautam.jayapal@gmail.com>
2023-01-04 10:30:00 +0530

    add cont hello 2023

---
+++ src/MKnezsConstructiveForcesTask.cpp
@@ -0,0 +1,104 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void dbg_out() { cerr << endl; }
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+
+template<typename T> T gcd(T a, T b) { return ( b ? __gcd(a, b) : a); }
+template<typename T> T lcm(T a, T b) { return (a * (b / gcd(a, b))); }
+
+#define forn(i, l, r) for (int i = (int)l; i < (int)r; ++i)
+#define fore(i, l, r) for (int i = (int)l; i <= (int)r; ++i)
+#define trav(i, a) for (auto& i : a)
+#define allit(a) a.begin(), a.end()
+#define sz(x) (int)(x).size()
+#define pb push_back
+#define shuf(v) shuffle(all(v), rng)
+#define umap unordered_map
+#define uset unordered_set
+#define imax INT_MAX
+#define imin INT_MIN
+
+#ifdef _GLIBCXX_DEBUG
+#define debug(...) cerr << "[DEBUG]: [" << #__VA_ARGS__ << "]:", dbg_out(__VA_ARGS__)
+#else
+#define debug(...)
+#endif
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pi;
+typedef vector<int> vi;
+typedef vector< vi > vvi;
+typedef vector< pi > vpi;
+
+mt19937 rng(chrono::high_resolution_clock::now().time_since_epoch().count());
+
+const char nl = '\n';
+// const ll MOD = 1e9+7;
+// const ld PI = acos(-1.0);
+
+void solve() {
+    // Algorithm:
+    // ==========
+    // If n is even, then resultant array is of the form
+    // [1, -1, 1, -1] adding upto sum 0.
+    //
+    // If n == 3, then it is impossible.
+    //
+    // If n is odd, then resultant array is of the form
+    // [a, b, a, b, a] where
+    // a = n / 2 - 1
+    // b = -(n / 2)
+    //
+    // Time: O(n)
+    // Space: O(1)
+
+    int t, n;
+    cin >> t;
+    while (t--) {
+        cin >> n;
+
+        if (n == 3) {
+            cout << "NO" << nl;
+            continue;
+        }
+
+        cout << "YES" << nl;
+        if (n & 1) {
+            bool flag = 1;
+            int a = n / 2 - 1;
+            int b = -(n / 2);
+            forn(_, 0, n) {
+                if (flag)
+                    cout << a << " ";
+                else
+                    cout << b << " ";
+                flag ^= 1;
+            }
+        } else {
+            int sign = 1;
+            forn(_, 0, n) {
+                cout << 1 * sign << " ";
+                sign *= -1;
+            }
+        }
+        cout << nl;
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    solve();
+
+#ifdef _GLIBCXX_DEBUG
+    cerr << endl << "finished in " << clock() * 1.0 / CLOCKS_PER_SEC << " sec" << endl;
+#endif
+
+    return 0;
+}
