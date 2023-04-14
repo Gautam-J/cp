1c539c93c0147ecadb88d9a3654264f4c32f9a90
Gautam J <gautam.jayapal@gmail.com>
2023-04-14 20:38:38 +0530

    add leetcode pattern probs

---
+++ src/searchInRotatedSortedArray.cpp
@@ -0,0 +1,93 @@
+// LT - Medium
+#include <bits/stdc++.h>
+using namespace std;
+
+void dbg_out() { cerr << endl; }
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+
+template<typename T> T gcd(T a, T b) { return ( b ? __gcd(a, b) : a); }
+template<typename T> T lcm(T a, T b) { return (a * (b / gcd(a, b))); }
+
+#define forn(i, l, r) for (int i = (int)l; i < (int)r; ++i)
+#define fore(i, l, r) for (int i = (int)l; i <= (int)r; ++i)
+#define trav(i, a) for (auto& i : a)
+#define allit(a) a.begin(), a.end()
+#define sz(x) (int)(x).size()
+#define pb push_back
+#define shuf(v) shuffle(allit(v), rng)
+#define umap unordered_map
+#define uset unordered_set
+#define imax INT_MAX
+#define imin INT_MIN
+
+#ifdef _GLIBCXX_DEBUG
+#define debug(...) cerr << "[DEBUG]: [" << #__VA_ARGS__ << "]:", dbg_out(__VA_ARGS__)
+#else
+#define debug(...)
+#endif
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pi;
+typedef vector<int> vi;
+typedef vector< vi > vvi;
+typedef vector< pi > vpi;
+
+mt19937 rng(chrono::high_resolution_clock::now().time_since_epoch().count());
+
+const char nl = '\n';
+// const ll MOD = 1e9+7;
+// const ld PI = acos(-1.0);
+
+void solve() {
+    // Algorithm:
+    // ==========
+    //
+    //
+    // Time: O(log n)
+    // Space: O(1)
+
+    int n, key, mid;
+    cin >> n;
+    vi a(n);
+    trav(i, a)
+        cin >> i;
+    cin >> key;
+
+    int l = 0, r = n - 1;
+    while (l <= r) {
+        mid = l + (r - l) / 2;
+        if (a[mid] == key) {
+            cout << mid << nl;
+            return;
+        }
+
+        if (a[l] <= a[mid]) {
+            if (key >= a[l] && key <= a[mid])
+                r = mid - 1;
+            else
+                l = mid + 1;
+        } else {
+            if (key >= a[mid] && key <= a[r])
+                l = mid + 1;
+            else
+                r = mid - 1;
+        }
+    }
+    cout << -1 << nl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    solve();
+
+#ifdef _GLIBCXX_DEBUG
+    cerr << endl << "finished in " << clock() * 1.0 / CLOCKS_PER_SEC << " sec" << endl;
+#endif
+
+    return 0;
+}
