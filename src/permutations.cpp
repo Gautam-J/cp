06b58df3c4c6521372d50f923045081e1862adfd
Gautam J <gautam.jayapal@gmail.com>
2025-01-24 18:24:35 +0530

    add permutations

---
+++ src/permutations.cpp
@@ -0,0 +1,116 @@
+/* CSES */
+
+/*
+Time limit: 1.00 s
+Memory limit: 512 MB
+
+A permutation of integers 1,2,\ldots,n is called beautiful if there are no adjacent elements whose difference is 1.
+Given n, construct a beautiful permutation if such a permutation exists.
+Input
+The only input line contains an integer n.
+Output
+Print a beautiful permutation of integers 1,2,\ldots,n. If there are several solutions, you may print any of them. If there are no solutions, print "NO SOLUTION".
+Constraints
+
+1 \le n \le 10^6
+
+Example 1
+Input:
+5
+
+Output:
+4 2 5 3 1
+Example 2
+Input:
+3
+
+Output:
+NO SOLUTION
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void dbg_out() { cerr << endl; }
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+
+template<typename T> T gcd(T a, T b) { return ( b ? __gcd(a, b) : a); }
+template<typename T> T lcm(T a, T b) { return (a * (b / gcd(a, b))); }
+
+#define forn(i, l, r) for (int i = (int)l; i < (int)r; ++i)
+#define fore(i, l, r) for (int i = (int)l; i <= (int)r; ++i)
+#define trav(i, a) for (auto& i : a)
+#define allit(a) a.begin(), a.end()
+#define sz(x) (int)(x).size()
+#define pb push_back
+#define shuf(v) shuffle(allit(v), rng)
+#define umap unordered_map
+#define uset unordered_set
+#define imax INT_MAX
+#define imin INT_MIN
+
+#ifdef _GLIBCXX_DEBUG
+#define debug(...) cerr << "[DEBUG]: [" << #__VA_ARGS__ << "]:", dbg_out(__VA_ARGS__)
+#else
+#define debug(...)
+#endif
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pi;
+typedef vector<int> vi;
+typedef vector< vi > vvi;
+typedef vector< pi > vpi;
+
+mt19937 rng(chrono::high_resolution_clock::now().time_since_epoch().count());
+
+const char nl = '\n';
+// const ll MOD = 1e9+7;
+// const ld PI = acos(-1.0);
+
+void solve() {
+    // Algorithm:
+    // ==========
+    // Print all even numbers, then all odd numbers.
+    //
+    // Time: O(n)
+    // Space: O(1)
+
+    int n;
+    cin >> n;
+
+    if (n == 1) {
+        cout << 1 << nl;
+        return;
+    }
+
+    if (n < 4) {
+        cout << "NO SOLUTION" << nl;
+        return;
+    }
+
+    for (int i = 2; i <= n; i += 2)
+        cout << i << ' ';
+
+    for (int i = 1; i <= n; i += 2)
+        cout << i << ' ';
+
+    cout << nl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    while(t--)
+        solve();
+
+#ifdef _GLIBCXX_DEBUG
+    cerr << endl << "finished in " << clock() * 1.0 / CLOCKS_PER_SEC << " sec" << endl;
+#endif
+
+    return 0;
+}
