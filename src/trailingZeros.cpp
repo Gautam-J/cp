455dfefc8631f8c2ef7237fd239450a98ceadc05
Gautam J <gautam.jayapal@gmail.com>
2025-01-25 10:37:27 +0530

    add trailing zeros in factorial

---
+++ src/trailingZeros.cpp
@@ -0,0 +1,102 @@
+/* CSES */
+
+/*
+Time limit: 1.00 s
+Memory limit: 512 MB
+
+Your task is to calculate the number of trailing zeros in the factorial n!.
+For example, 20!=2432902008176640000 and it has 4 trailing zeros.
+Input
+The only input line has an integer n.
+Output
+Print the number of trailing zeros in n!.
+Constraints
+
+1 \le n \le 10^9
+
+Example
+Input:
+20
+
+Output:
+4
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void dbg_out() { cerr << endl; }
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+
+template<typename T> T gcd(T a, T b) { return ( b ? __gcd(a, b) : a); }
+template<typename T> T lcm(T a, T b) { return (a * (b / gcd(a, b))); }
+
+#define forn(i, l, r) for (int i = (int)l; i < (int)r; ++i)
+#define fore(i, l, r) for (int i = (int)l; i <= (int)r; ++i)
+#define trav(i, a) for (auto& i : a)
+#define allit(a) a.begin(), a.end()
+#define sz(x) (int)(x).size()
+#define pb push_back
+#define shuf(v) shuffle(allit(v), rng)
+#define umap unordered_map
+#define uset unordered_set
+#define imax INT_MAX
+#define imin INT_MIN
+
+#ifdef _GLIBCXX_DEBUG
+#define debug(...) cerr << "[DEBUG]: [" << #__VA_ARGS__ << "]:", dbg_out(__VA_ARGS__)
+#else
+#define debug(...)
+#endif
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pi;
+typedef vector<int> vi;
+typedef vector< vi > vvi;
+typedef vector< pi > vpi;
+
+mt19937 rng(chrono::high_resolution_clock::now().time_since_epoch().count());
+
+const char nl = '\n';
+// const ll MOD = 1e9+7;
+// const ld PI = acos(-1.0);
+
+void solve() {
+    // Algorithm:
+    // ==========
+    // Count number of times 5 appears as a factor in n!
+    // floor(n/5) + floor(n/25) + floor(n/125) + ... + 0
+    //
+    // Time: O(log5 n)
+    // Space: O(1)
+
+    int n;
+    cin >> n;
+
+    int res = 0;
+    ll divisor = 5;
+    while (divisor <= n) {
+        res += n / divisor;
+        divisor *= 5;
+    }
+
+    cout << res << nl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    while(t--)
+        solve();
+
+#ifdef _GLIBCXX_DEBUG
+    cerr << endl << "finished in " << clock() * 1.0 / CLOCKS_PER_SEC << " sec" << endl;
+#endif
+
+    return 0;
+}
