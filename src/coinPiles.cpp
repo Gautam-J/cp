1cb0009c988ca5c4e0354e7275eae4e16ee822fa
Gautam J <gautam.jayapal@gmail.com>
2025-01-25 11:05:36 +0530

    add coin piles

---
+++ src/coinPiles.cpp
@@ -0,0 +1,108 @@
+// CSES
+
+/*
+Time limit: 1.00 s
+Memory limit: 512 MB
+
+You have two coin piles containing a and b coins. On each move, you can either remove one coin from the left pile and two coins from the right pile, or two coins from the left pile and one coin from the right pile.
+Your task is to efficiently find out if you can empty both the piles.
+Input
+The first input line has an integer t: the number of tests.
+After this, there are t lines, each of which has two integers a and b: the numbers of coins in the piles.
+Output
+For each test, print "YES" if you can empty the piles and "NO" otherwise.
+Constraints
+
+1 \le t \le 10^5
+0 \le a, b \le 10^9
+
+Example
+Input:
+3
+2 1
+2 2
+3 3
+
+Output:
+YES
+NO
+YES
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void dbg_out() { cerr << endl; }
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+
+template<typename T> T gcd(T a, T b) { return ( b ? __gcd(a, b) : a); }
+template<typename T> T lcm(T a, T b) { return (a * (b / gcd(a, b))); }
+
+#define forn(i, l, r) for (int i = (int)l; i < (int)r; ++i)
+#define fore(i, l, r) for (int i = (int)l; i <= (int)r; ++i)
+#define trav(i, a) for (auto& i : a)
+#define allit(a) a.begin(), a.end()
+#define sz(x) (int)(x).size()
+#define pb push_back
+#define shuf(v) shuffle(allit(v), rng)
+#define umap unordered_map
+#define uset unordered_set
+#define imax INT_MAX
+#define imin INT_MIN
+
+#ifdef _GLIBCXX_DEBUG
+#define debug(...) cerr << "[DEBUG]: [" << #__VA_ARGS__ << "]:", dbg_out(__VA_ARGS__)
+#else
+#define debug(...)
+#endif
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pi;
+typedef vector<int> vi;
+typedef vector< vi > vvi;
+typedef vector< pi > vpi;
+
+mt19937 rng(chrono::high_resolution_clock::now().time_since_epoch().count());
+
+const char nl = '\n';
+// const ll MOD = 1e9+7;
+// const ld PI = acos(-1.0);
+
+void solve() {
+    // Algorithm:
+    // ==========
+    // Total number of coins should be divisible by 3.
+    // 2 * min(a, b) >= max(a, b) else we will run out of coins in one pile.
+    //
+    // Time: O(1)
+    // Space: O(1)
+
+    ll a, b;
+    cin >> a >> b;
+
+    if ((a + b) % 3) {
+        cout << "NO" << nl;
+        return;
+    }
+
+    cout << ((2 * a >= b && 2 * b >= a) ? "YES" : "NO") << nl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+    while(t--)
+        solve();
+
+#ifdef _GLIBCXX_DEBUG
+    cerr << endl << "finished in " << clock() * 1.0 / CLOCKS_PER_SEC << " sec" << endl;
+#endif
+
+    return 0;
+}
